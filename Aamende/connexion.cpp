e0bc23262f2ce06bcde1f092d122596ca986b569
mohamed <mohamed.jomaa@esprit.tn>
2020-11-29 19:17:57 +0100

    first one commit

---
+++ Aamende/connexion.cpp
@@ -0,0 +1,17 @@
+#include "connexion.h"
+#include "QSqlDatabase"
+
+connexion::connexion()
+{
+
+}
+bool connexion::createConnexion(){
+   bool test=false;
+   QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+   db.setDatabaseName("test");
+   db.setUserName("mohamed");
+   db.setPassword("mohamed00");
+   if(db.open())
+test=true;
+return test;
+}
